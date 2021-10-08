bfd7dd8b721b7126171f878edc89ed28bc0fb60f
CheilaMarqs <91612535+CheilaMarqs@users.noreply.github.com>
2021-10-08 18:03:33 -0400

    Questao1

---
+++ Questao1.vhd
@@ -0,0 +1,14 @@
+library STD;
+USE STANDARD.ALL;
+
+entity portas is
+port(
+  A, B : IN BIT;
+  X : OUT BIT;
+     );
+end portas;
+
+architecture circuito of portas is
+Begin
+  X <= NOT(NOT(A OR B));
+end circuito;
